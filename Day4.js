d33402af9cad489b33fc364a4b1e5fb6429d9dbc
tiashah1810 <tiashah101317@gmail.com>
2025-08-27 21:08:49 +0530

    Added Day4.js minitask4

--- Day4.js
+++ Day4.js
@@ -1,4 +1,3 @@
-
 const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
@@ -7,3 +6,4 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
+
